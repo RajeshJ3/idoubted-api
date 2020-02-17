8d9b29ecbaba9c7b5520961e5afa88c42fa89795
Rajesh Joshi <joshirajesh448@gmail.com>
2020-02-18 02:11:56 +0530

    Added Dynamic loading to frontend

--- news/urls.py
+++ news/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path, include
-from rest_framework import routers
-from .views import NewsViewSet, gadgetsnow_generate_news, news18_generate_news
-
-router = routers.DefaultRouter()
-router.register(r"", NewsViewSet)
+from .views import gadgetsnow_generate_news, news18_generate_news, news_list
 
 urlpatterns = [
     path('generate/gadgetsnow/', gadgetsnow_generate_news),
     path('generate/news18/', news18_generate_news),
-    path('', include(router.urls)),
+    path('', news_list),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework import status
+from django.db.models import Q
 from .serializers import NewsSerializer
 from .models import News
 from .news.gadgetsnow import gadgetsnow_generate
@@ -14,7 +19,31 @@ def news18_generate_news(request):
     news18_generate()
 
 
-class NewsViewSet(viewsets.ModelViewSet):
-    queryset = News.objects.filter(publish=True).order_by('-time')[0:20]
-    serializer_class = NewsSerializer
-    lookup_field = 'slug'
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def news_list(request):
+    index = request.data.get("index")
+    category = request.data.get("category")
+
+    if category == "Tech news":
+        category = "tech"
+    else:
+        category = False
+
+    if index == -1:
+        if category:
+            news = News.objects.filter(Q(publish=True) & Q(
+                category=category)).order_by('-time')[0:5]
+        else:
+            news = News.objects.filter(publish=True).order_by('-time')[0:5]
+        serializer = NewsSerializer(news, many=True)
+    else:
+        if category:
+            news = News.objects.filter(Q(publish=True) & Q(
+                pk__lt=index) & Q(category=category)).order_by('-time')[0:5]
+        else:
+            news = News.objects.filter(Q(publish=True) & Q(
+                pk__lt=index)).order_by('-time')[0:5]
+
+    serializer = NewsSerializer(news, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
